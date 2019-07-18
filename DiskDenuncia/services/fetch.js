82270348e5a5f510a5de5f75f0dcf3f8eaf53941
jlucfarias <lucas_ennabledmu2@hotmail.com>
2019-07-18 17:50:03 -0300

    add api fetch model

---
+++ DiskDenuncia/services/fetch.js
@@ -0,0 +1,210 @@
+const URI = 'https://api-dd.herokuapp.com';
+const apiPath = '/api/';
+
+export default {
+  async getAtuacao() {
+    try {
+      let response = await fetch(URI + apiPath + 'atuacaos');
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async postAtuacao(data) {
+    try {
+      let response = await fetch(URI + apiPath + 'atuacaos', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json());
+      console.log('Atuação adicionada com sucesso');
+    } catch(e) {
+      console.error(e);
+    }
+  },
+  
+  async getCadastro(id) {
+    try {
+      let response = await fetch(URI + apiPath + 'cadastros/' + id);
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async postCadastro(data) {
+    try {
+      let response = await fetch(URI + apiPath + 'cadastros', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json());
+      console.log('Cadastro adicionada com sucesso');
+    } catch(e) {
+      console.error(e);
+    }
+  },
+  
+  async getCategorias() {
+    try {
+      let response = await fetch(URI + apiPath + 'categoria');
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async getCategoria(id) {
+    try {
+      let response = await fetch(URI + apiPath + 'categoria/' + id);
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async postCategoria(data) {
+    try {
+      let response = await fetch(URI + apiPath + 'categoria', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json());
+      console.log('Categoria adicionada com sucesso');
+    } catch(e) {
+      console.error(e);
+    }
+  },
+
+  async getDenuncia(id) {
+    try {
+      let response = await fetch(URI + apiPath + 'denuncias/' + id);
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async getDenuncias() {
+    try {
+      let response = await fetch(URI + apiPath + 'denuncias');
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async getDenunciasByCategoria(categoria_id) {
+    try {
+      let response = await fetch(URI + apiPath + 'denuncias/' + categoria_id);
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async putDenuncia(data) {
+    try {
+      let response = await fetch(URI + apiPath + 'denuncias', {
+        method: 'PUT',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json());
+      console.log('Denúncia editada com sucesso');
+    } catch(e) {
+      console.error(e);
+    }
+  },
+
+  async deleteDenuncia(id) {
+    try {
+      let response = await fetch(URI + apiPath + 'denuncias/' + id, {
+        method: 'DELETE',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json());
+      console.log('Denúncia deletada com sucesso');
+    } catch(e) {
+      console.error(e);
+    }
+  },
+
+  async postDenuncia(data) {
+    try {
+      let response = await fetch(URI + apiPath + 'denuncias', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json());
+      console.log('Denúncia adicionada com sucesso');
+    } catch(e) {
+      console.error(e);
+    }
+  },
+
+  async getEnderecos() {
+    try {
+      let response = await fetch(URI + apiPath + 'enderecos');
+      let responseJsonData = await response.json();
+      return responseJsonData;
+    } catch(e) {
+      console.log(e)
+    }
+  },
+
+  async postEnderecos(data) {
+    try {
+      let response = await fetch(URI + apiPath + 'enderecos', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => response.json());
+      console.log('Enderecos adicionada com sucesso');
+    } catch(e) {
+      console.error(e);
+    }
+  },
+}
