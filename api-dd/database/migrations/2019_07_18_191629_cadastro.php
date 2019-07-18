c6c337106bdd2b236247b51c504d43f81b3b2c30
GuilhermeOtsugua <guilhermebartolis@gmail.com>
2019-07-18 18:21:43 -0300

    Mais correções nas migrações

--- api-dd/database/migrations/2019_07_18_191629_cadastro.php
+++ api-dd/database/migrations/2019_07_18_191629_cadastro.php
@@ -18,19 +18,11 @@ class Cadastro extends Migration
             $table->string('id_agente');
             $table->string('senha');
 
-<<<<<<< HEAD
-            //$table->unsignedBigInteger('atuacao_id')->unsigned();
-            //$table->foreign('atuacao_id')->references('id')->on('atuacao');
-            
-            //$table->unsignedBigInteger('denuncia_id')->unsigned();
-            //$table->foreign('denuncia_id')->references('id')->on('denuncia');
-=======
             // $table->unsignedBigInteger('atuacao_id')->unsigned();
             // $table->foreign('atuacao_id')->references('id')->on('atuacao');
             
             // $table->unsignedBigInteger('denuncia_id')->unsigned();
             // $table->foreign('denuncia_id')->references('id')->on('denuncia');
->>>>>>> d35fd2fc1b10508a43ce50840ee9b5e5ac799d5b
         });
     }
 
