09ae5a599d3c29eff09467373994d8365269df74
Lucas Moura <lluckymou@gmail.com>
2019-07-18 20:28:29 -0300

    Mapa

--- DiskDenuncia/pages/map.js
+++ DiskDenuncia/pages/map.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-//import WebViewLeaflet from "react-native-webview-leaflet";
+import MapView, { PROVIDER_DEFAULT, MAP_TYPES, Marker } from 'react-native-maps';
 
-export default class Home extends React.Component {
-    render() {
-        return null;
-    }
+const { width, height } = Dimensions.get('window');
+const aspectRatio = width / height;
+const latitDelta = 0.0922;
+const longiDelta = latitDelta * aspectRatio;
+
+function getInitialState() {
+  return {
+    region: {
+      latitude: -5.812691,
+      longitude: -35.205100,
+      latitudeDelta: latitDelta,
+      longitudeDelta: longiDelta,
+    },
+  };
+}
+
+export default function App() {
+  this.state = getInitialState();
+  this.onRegionChange = function() {
+  };
+  return (
+    <View style={{flex: 1}}>
+      <MapView
+        style={{flex: 1}}
+        provider={null}
+        mapType={MAP_TYPES.STANDARD}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+      >
+        <Marker
+          coordinate={{
+            latitude: -5.857807, 
+            longitude: -35.203667,
+          }}
+          image={require('../assets/invasao.png')}
+        />
+      </MapView>
+    </View>
+  );
 }
