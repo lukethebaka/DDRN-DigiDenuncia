4694933e4c6e97220810aaa14966322ff2894758
GuilhermeOtsugua <guilhermebartolis@gmail.com>
2019-07-19 14:42:26 -0300

    Verificando /index dos Controllers!

--- api-dd/app/Http/Controllers/Api/AtuacaosController.php
+++ api-dd/app/Http/Controllers/Api/AtuacaosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use APp\Atuacao;
+use App\Atuacao;
 
 class AtuacaosController extends Controller
 {
